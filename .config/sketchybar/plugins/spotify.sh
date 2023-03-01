c7214cbf239261e7d3c6e76e80c5ff2bf2601402
Bryan Yang <bryan.yang255@gmail.com>
2023-03-01 13:14:40 +1100

    fix stuff

--- .config/sketchybar/plugins/spotify.sh
+++ .config/sketchybar/plugins/spotify.sh
@@ -10,7 +10,7 @@ update ()
   PLAYING=1
   if [ "$(echo "$INFO" | jq -r '.["Player State"]')" = "Playing" ]; then
     PLAYING=0
-    TRACK="$(echo "$INFO" | jq -r .Name | cut -c1-20)"
+    TRACK="$(echo "$INFO" | jq -r .Name | cut -c1-30)"
     ARTIST="$(echo "$INFO" | jq -r .Artist | cut -c1-20)"
     ALBUM="$(echo "$INFO" | jq -r .Album | cut -c1-20)"
     SHUFFLE=$(osascript -e 'tell application "Spotify" to get shuffling')
