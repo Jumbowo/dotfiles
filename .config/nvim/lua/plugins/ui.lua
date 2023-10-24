36016c2167434233d91da3c1e134976eb39e7c09
Bryan Yang <bryan.yang255@gmail.com>
2023-10-24 17:43:05 +1100

    no more bufferline

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,36 +1,25 @@
 return {
-  -- Bufferline
-  {
-    "akinsho/bufferline.nvim",
-      config = function()
-        require("bufferline").setup {
-          options = {
+    -- Lualine
+    {
+        "nvim-lualine/lualine.nvim",
+        config = function()
+            require("lualine").setup {
+                options = {
+                    globalstatus = true,
+                    theme = "auto",
+                },
+            }
+        end
+    },
 
-          },
-        }
-      end
-  },
-
-  -- Lualine
-  {
-    "nvim-lualine/lualine.nvim",
-      config = function()
-        require("lualine").setup {
-          options = {
-            globalstatus = true,
-          },
-        }
-      end
-  },
-
-  -- Telescope
-  {
-    "nvim-telescope/telescope.nvim",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-      },
-      cmd = "Telescope",
-      version = false,
-      opts = { }
+    -- Telescope
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        cmd = "Telescope",
+        version = false,
+        opts = { }
     }
 }
