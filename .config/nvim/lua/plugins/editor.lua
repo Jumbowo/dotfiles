ac6db548d1b3903fd5ce84f2a7776b04d7f12c61
Bryan Yang <bryan.yang255@gmail.com>
2023-11-23 16:02:40 +1100

    better autopairs

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,39 +1,39 @@
 return {
-    -- Auto closing brackets
-    {
-        "echasnovski/mini.pairs",
-        config = function(_, opts)
-            require("mini.pairs").setup(opts)
-        end,
-    },
+  -- Auto closing brackets
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {}
+  },
 
-    -- Comment
-    {
-        "echasnovski/mini.comment",
-        config = function(_, opts)
-            require("mini.comment").setup(opts)
-        end,
-    },
+  -- Comment
+  {
+    "echasnovski/mini.comment",
+    config = function(_, opts)
+      require("mini.comment").setup(opts)
+    end,
+  },
 
-    -- Indent blankline
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        opts = {
-            indent = {
-                char = "│"
-            },
-            scope = {
-                enabled = false
-            },
-        },
-        config = function(_, opts)
-            require("ibl").setup(opts)
-        end
+  -- Indent blankline
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      indent = {
+        -- char = "▏"
+        char = "│"
+      },
+      scope = {
+        enabled = false
+      },
     },
+    config = function(_, opts)
+      require("ibl").setup(opts)
+    end
+  },
 
-    -- Rainbow pairs
-    {
-        "HiPhish/nvim-ts-rainbow2",
-    },
+  -- Rainbow pairs
+  {
+    "HiPhish/nvim-ts-rainbow2",
+  },
 }
