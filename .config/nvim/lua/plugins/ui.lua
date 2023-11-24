d44baa5d509d2690418725080161e6c873544953
Bryan Yang <bryan.yang255@gmail.com>
2023-11-24 11:49:05 +1100

    add nvim-tree

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,45 +1,56 @@
 return {
-    -- Lualine
-    {
-        "nvim-lualine/lualine.nvim",
-        config = function()
-            require("lualine").setup {
-                options = {
-                    globalstatus = true,
-                    theme = "auto",
-                },
-            }
-        end
-    },
-
-    -- Telescope
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
+  -- Lualine
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup {
+        options = {
+          globalstatus = true,
+          theme = "auto",
         },
-        cmd = "Telescope",
-        version = false,
-        opts = { }
-    },
+      }
+    end
+  },
 
-    -- Gitsigns
-    {
-        "lewis6991/gitsigns.nvim",
-        opts = { }
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
     },
+    cmd = "Telescope",
+    version = false,
+    opts = { }
+  },
 
-    -- Aerial
-    {
-        'stevearc/aerial.nvim',
-        opts = {
-            layout = {
-                max_width = 0.3,
-                min_width = 0.2,
-                default_direction = "right",
-                placement = "edge",
-            },
-            attach_mode = "global",
-        },
+  -- Gitsigns
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = { }
+  },
+
+  -- Aerial
+  {
+    'stevearc/aerial.nvim',
+    opts = {
+      layout = {
+        max_width = 35,
+        min_width = 35,
+        default_direction = "right",
+        placement = "edge",
+      },
+      attach_mode = "global",
+    },
+  },
+
+  -- Nvim Tree
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      view = {
+        width = 35,
+      }
     }
+  }
+
 }
